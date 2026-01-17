ca0db08801f2f2345794143d899f371c7b09ed3d
gursoyasin <gursoyreal@gmail.com>
2026-01-17 16:01:54 +0300

    Initial Commit

---
+++ FirsatAvcisi/FirsatAvcisiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirsatAvcisiApp.swift
+//  FirsatAvcisi
+//
+//  Created by yacN on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FirsatAvcisiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
